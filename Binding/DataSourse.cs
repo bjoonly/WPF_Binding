d432d4bb3f03d0b6ed2952c2713b9d41cc5369ee
bjoonly <elumnaya@gmail.com>
2020-12-03 16:21:01 +0200

    Init

---
+++ Binding/DataSourse.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Binding
+{
+
+    public class DataSourse:INotifyPropertyChanged
+    {
+        private readonly ICollection<ColorModel> colors = new ObservableCollection<ColorModel>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IEnumerable<ColorModel> Colors => colors;
+
+
+        private ColorModel selectedColor;
+        public ColorModel SelectedColor {
+            get { return selectedColor; }
+            set { 
+                if (value != selectedColor ){
+                    selectedColor = value;
+                    OnPropertyChanged();
+
+                } }
+        }
+        public DataSourse()
+        {
+           SelectedColor = new ColorModel();
+
+        }
+
+
+        public void AddSelectedColor()
+        {
+            if (SelectedColor != null)
+                colors.Add((ColorModel)SelectedColor.Clone());
+         
+        }
+
+        public void RemoveSelectedColor()
+        {
+            if (SelectedColor != null)
+            {
+
+                colors.Remove(SelectedColor);
+                SelectedColor = new ColorModel();
+
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Binding/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Binding
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+
+
+    public partial class MainWindow : Window
+    {
+        private DataSourse ds = new DataSourse();
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            this.DataContext = ds;
+        }
+
+        private void AddSelectedColor(object sender, RoutedEventArgs e)
+        {
+            ds.AddSelectedColor();
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((listBox).SelectedItem != null)
+                addButton.IsEnabled = false;
+            else
+                addButton.IsEnabled = true;
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            ds.RemoveSelectedColor();
+            addButton.IsEnabled = true;
+            
+
+        }
+    }
+}
